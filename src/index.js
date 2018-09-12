eac9071bbfd9f0706e0cbdaf3c62ab6a9c5fb953
EvilaMany <lineman98@yandex.ru>
2018-09-12 18:13:05 +0300

    solution of seventh task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+  var result = 0;
+
+  for(var dev = 5; dev <= number;dev*=5)
+  {
+  	result += Math.floor(number / dev)
+  }
+  return result;
 }
